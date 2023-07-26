a6aa1033c17f504bfc9db81a9620a945a5dfbaaa
Carlos Herraiz <ciherraiz@gmail.com>
2023-07-26 19:04:38 +0200

    remove adapter.py

--- ttapi/session.py
+++ ttapi/session.py
@@ -1,48 +1,108 @@
-import logging
+from json import JSONDecodeError
+import aiohttp
 from typing import Any, Optional
-#from config.config import tastytrade as ttcfg
-#from config.config import foo
-from ttapi.adapter import Adapter
+from ttapi.models import RequestResult # type: ignore
+from ttapi.exceptions import TastyTradeException # type: ignore
 from ttapi.config import tastytrade as ttcfg
+from ttapi.config import logger
 
 class Session:
-    """
-    Has the configuration to interact with API
-    """
-    def __init__(self, user: str, password: str, is_certification: bool = False, logger: logging.Logger | None = None) -> None:
+
+    def __init__(self, 
+                 user: str, 
+                 password: str, 
+                 is_production: bool = True, 
+                 remember_me: bool = False, 
+                 remember_token: Optional[str] = None,
+                 two_factor_authentitation: Optional[str] = None) -> None:
         """
         Create a session
-        :param login: tastytrade username or email
-        :param password: tastytrade password or a remember token obtained previously
+        :param user: tastytrade username or email
+        :param password: tastytrade password
+        :param is_production: production or developer (certification) environment
+        :param remember_me: create or not a remember token
+        :param remember_token: previously creatd token
+        :param two_factor_authentitation: is 2FA is enabled, this is de code sent
         """
         self._session_token: str = ''
 
-        payload: dict[str, Any] = {
+        self._payload: dict[str, Any] = {
             "login": user,
-            "password": password,
-            "remember-me": ttcfg['authentication']['remember_me']
+            "remember-me": remember_me
         }
 
+        if password is not None:
+            self._payload['password'] = password
+        elif remember_token is not None:
+            self._payload['remember-token'] = remember_token
+        else:
+            raise TastyTradeException('You must provide a password or a remember token')
+
         # base url selection
-        self._base_url = ttcfg['certification']['url'] if is_certification else ttcfg['production']['url']
-        self._base_wss = ttcfg['certification']['wss'] if is_certification else ttcfg['production']['wss']
+        self._base_url = ttcfg['production']['url'] if is_production else ttcfg['certification']['url']
+        self._headers = ttcfg['authentication']['headers'].copy()
 
-        self._adapter = Adapter(self._base_url)
-        self._user = user
-        self._password = password
+        if two_factor_authentitation:
+            self._headers['X-Tastyworks-OTP'] = two_factor_authentitation
 
-        if ttcfg['authentication']['mfa'] == True:
-            two_factor_auth = input('2FA: ')
-            self._adapter.add_header('X-Tastyworks-OTP', two_factor_auth)
-   
-        response = self._adapter.do('POST', '/sessions', json=payload)
+
+    async def _http_request(self, http_method: str, url: str, endpoint: str, json: dict[str, str] = {}, data: str = None, params: dict[str, str] = {}) ->  RequestResult:
+        """
+        Do a http request
+        """
+        async with aiohttp.ClientSession() as session: 
+            async with session.request(method=http_method, url=url + endpoint, headers=self._headers, json=json, data=data, params=params) as response:           
+                # Deserialize JSON output to Python object if there is some content
+                #if response.content:
+                try:
+                    data_out = await response.json()
+                except (ValueError, JSONDecodeError) as e:
+                    logger.exception('Adapter exception')
+                    raise TastyTradeException('Decoding JSON failed') from e
+                #else:
+                #    data_out = ''
+                
+                is_success =  299 >= response.status >= 200
+                
+                logger.debug(f'method={http_method}, url={url + endpoint} status_code={response.status}, message={response.reason}')
+
+                if is_success:
+                    return RequestResult(int(response.status), message=response.reason, data=data_out)
+                else:
+                    raise TastyTradeException('HTTP request failed')
+
+    @classmethod
+    async def create(cls, 
+                     user: str, 
+                    password: str, 
+                    is_production: bool = True, 
+                    remember_me: bool = False, 
+                    remember_token: Optional[str] = None,
+                    two_factor_authentitation: Optional[str] = None):
+        """
+        Factory methon to create a session
+        :param user: tastytrade username or email
+        :param password: tastytrade password
+        :param is_production: production or developer (certification) environment
+        :param remember_me: create or not a remember token
+        :param remember_token: previously creatd token
+        :param two_factor_authentitation: is 2FA is enabled, this is de code sent
+        """
+        
+        self = cls(user, password, is_production, remember_me, remember_token, two_factor_authentitation)
+        
+        # TastyTrade API session
+        response = await self._http_request('POST', self._base_url, '/sessions', json=self._payload)
 
         self._session_token = response.data['data']['session-token']
-        self._remember_token: Optional[str] = response.data['data']['remember-token'] if ttcfg['authentication']['remember_me'] else None
+        self._remember_token: Optional[str] = response.data['data']['remember-token'] if remember_me else None
         self._external_id: str = response.data['data']['user']['external-id']
-
         # add the header to use for API requests
-        self._adapter.add_header('Authorization', self._session_token)
+        self._headers['Authorization'] = self._session_token
+
+        # DxFeed data API session
+
+        return self
 
     def request(self, http_method: str, endpoint: str, json: dict[str, str] = {}, data: str = None, params: dict[str, str] = {}):
         return self._adapter.do(http_method, endpoint, json, data, params)
