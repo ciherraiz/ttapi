a3780ceb83d168b5d73705d078ef9540e5ef2bcd
Carlos Herraiz <ciherraiz@gmail.com>
2023-08-07 13:38:59 +0200

    Add listen method

--- ttapi/dxlink_streamer.py
+++ ttapi/dxlink_streamer.py
@@ -7,7 +7,14 @@ from ttapi.config import logger
 from ttapi.config import tastytrade as cfg
 from ttapi.exceptions import TastyTradeException
 from ttapi.session import Session
-from ttapi.models import ChannelState, EventType
+from ttapi.dxlink_models import ChannelState, EventType, Quote, JsonDataclass
+
+# Channels id
+QUOTE_CH = 1
+PROFILE_CH = 2
+SUMMARY_CH = 3
+TRADE_CH = 4
+GREEKS_CH = 5
 
 class DXLinkStreamer:
     """
@@ -17,21 +24,20 @@ class DXLinkStreamer:
 
     """
     def __init__(self, session: Session):
-        #: The active session used to initiate the streamer or make requests
-        self.session: Session = session
-        self._channels = {}
+        self._session: Session = session
         
         self._keep_alive_timeout = cfg['dxlink']['keep_alive_timeout']
 
-        self._counter = 0
-        self._lock: Lock = Lock()
-        self._queue: Queue = Queue()
-        self._queue_candle: Queue = Queue()
+        self._channels = {QUOTE_CH: {'event': EventType.QUOTE, 'queue': Queue()},
+                          PROFILE_CH: {'event': EventType.PROFILE},
+                          SUMMARY_CH: {'event': EventType.SUMMARY},
+                          TRADE_CH: {'event': EventType.TRADE},
+                          GREEKS_CH: {'event': EventType.GREEKS}}
         
         self._authorized: bool = False
-        #self._handshake_done = False
         
         self._connect_task = asyncio.create_task(self._connect())
+        
 
     @classmethod
     async def create(cls, session: Session) -> 'DXLinkStreamer':
@@ -53,34 +59,20 @@ class DXLinkStreamer:
         """
         # Get DxLink API Quote Token
         # Quote streamer tokens are valid for 24 hours
-        response = await self.session.tt_request('GET', '/api-quote-tokens')
-        token = response.data['data']['token']
-        url = response.data['data']['dxlink-url']
+        response = await self._session.tt_request('GET', '/api-quote-tokens')
+        self._token = response.data['data']['token']
+        uri = response.data['data']['dxlink-url']
 
         # Connect to the websocket server
         async with aiohttp.ClientSession() as session:
-            async with session.ws_connect(url, proxy=self.session.proxy) as ws:
+            async with session.ws_connect(uri, proxy=self._session.proxy) as ws:
                 self._websocket = ws
                 await self._setup()
-                await self._authentication(token)
                 # Main loop to handle incoming messages
                 while True:
                     async for raw_msg in ws:
                         message = json.loads(raw_msg.data)
-                        logger.debug(f'message {message}')
-                        if not self._authorized:
-                            if message['type'] == 'AUTH_STATE' and message['state'] == 'AUTHORIZED':
-                                # the connection is authorized
-                                self._authorized = True
-                                # start connection maintenance by sendig keep alive message periodicaly
-                                self._keep_alive_task = asyncio.create_task(self._keep_alive(self._keep_alive_timeout))
-                            
-                        if message['type'] == ChannelState.OPENED.value:
-                            for key, values in self._channels.items():
-                                if values['channel'] == message['channel']:
-                                    self._channels[key]['state'] = ChannelState.OPENED
-                                    break
-
+                        await self._on_message(message)
 
 
     async def _setup(self) -> None:
@@ -90,12 +82,28 @@ class DXLinkStreamer:
                    "acceptKeepaliveTimeout": 60, 
                    "version": cfg['dxlink']['version'] }
         await self._websocket.send_json(message)
-
-    async def _authentication(self, token) -> None:
-        message = {"type": "AUTH",
+        
+    async def _on_message(self, message_rcv) -> None:
+        logger.debug(f'message {message_rcv}')
+        message = None
+        match message_rcv['type']:
+            case ChannelState.SETUP.value:
+                message = {"type": "AUTH",
                    "channel": 0,
-                   "token": token}
-        await self._websocket.send_json(message)
+                   "token": self._token}
+            case ChannelState.AUTHORIZATION.value:
+                if message_rcv['state'] == 'AUTHORIZED':
+                    self._authorized = True
+                    self._keep_alive_task = asyncio.create_task(self._keep_alive(self._keep_alive_timeout))
+            case ChannelState.OPENED.value:
+                self._channels[message_rcv['channel']]['state'] = ChannelState.OPENED
+            case ChannelState.CONFIG.value:
+                self._channels[message_rcv['channel']]['state'] = ChannelState.CONFIG
+            case ChannelState.DATA.value:
+                await self._map_message(message_rcv) 
+                
+        if message is not None:
+            await self._websocket.send_json(message)
 
     async def _keep_alive(self, timeout: int = 10) -> None:
         message = {"type": "KEEPALIVE",
@@ -105,20 +113,22 @@ class DXLinkStreamer:
             await asyncio.sleep(timeout)
     
     async def subscribe_quote(self, symbol) -> None:
-        if not self._channels.get(EventType.QUOTE):
-            # Create a new channel for quotes
-            self._channels[EventType.QUOTE] = {'channel': 1, 'state': ChannelState.REQUEST, 'symbol': symbol}
-            message = {"type": ChannelState.REQUEST.value,
-                       "channel": self._channels[EventType.QUOTE]['channel'],
-                       "service": "FEED",
-                       "parameters": {"contract": "AUTO"}}
-            await self._websocket.send_json(message)
+       
+        # Create a new channel for quotes
+        self._channels[QUOTE_CH]['state'] = ChannelState.REQUEST
+        self._channels[QUOTE_CH]['symbol'] = symbol
 
-            while self._channels[EventType.QUOTE]['state'] == ChannelState.REQUEST:
-                await asyncio.sleep(0)
+        message = {"type": ChannelState.REQUEST.value,
+                    "channel": 1,
+                    "service": "FEED",
+                    "parameters": {"contract": "AUTO"}}
+        await self._websocket.send_json(message)
+
+        while self._channels[QUOTE_CH]['state'] == ChannelState.REQUEST:
+            await asyncio.sleep(0)
 
         message = {"type": ChannelState.SUBSCRIPTION.value,
-                   "channel": self._channels[EventType.QUOTE]['channel'],
+                   "channel": QUOTE_CH,
                    "add": [{"symbol": symbol, "type": EventType.QUOTE.value}]}
         
         await self._websocket.send_json(message)
@@ -128,9 +138,35 @@ class DXLinkStreamer:
         Closes the websocket connection and cancels the keep alive task.
         """
         # Close Channels
-        for channel in self._channels.values():
+        for channel in self._channels.keys():
             message = {"type": ChannelState.CANCEL.value,
-                       "channel": channel['channel']}
+                       "channel": channel}
             await self._websocket.send_json(message)
         self._connect_task.cancel()
         self._keep_alive_task.cancel()
+
+    
+    async def listen_quotes(self) -> AsyncIterator[JsonDataclass]:
+        while True:
+            data = await self._channels[QUOTE_CH]['queue'].get()
+            yield data
+
+    
+    async def _map_message(self, message) -> None:
+        """
+        Takes the raw JSON data, parses the events and places them into their
+        respective queues.
+
+        :param message: raw JSON data from the websocket
+        """
+
+        # parse type or warn for unknown type
+        channel = message['channel']
+        data = message['data'][0]
+        match channel:
+            case QUOTE_CH:
+                event = Quote(**data)
+                #data = Quote.from_stream(message['data'])
+                
+        
+        await self._channels[channel]['queue'].put(event)
